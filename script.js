7dc02a73e24cef11910e7042d17ed8f8dba92f05
Gavin-Vaughan <gavinvaughan1@outlook.com>
2024-07-21 11:11:34 -0400

    Added JavaScript code for the button in index.html

---
+++ script.js
@@ -0,0 +1,4 @@
+var button = document.getElementById('thebutton');
+button === null || button === void 0 ? void 0 : button.addEventListener('click', function () {
+    alert('Hello, thanks for clicking me');
+});
---
+++ script.ts
@@ -0,0 +1,1 @@
+//Code already converted to JavaScript
